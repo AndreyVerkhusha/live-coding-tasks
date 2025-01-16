280f43cf71cdc3aa247e66f9520e23b06abbe5d6
AndreyVerkhusha <135759268+AndreyVerkhusha@users.noreply.github.com>
2025-01-16 16:11:01 +0300

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,10 +1,28 @@
 /* написать собственную реализацию метода у массивов .map */
+const arr = [1,2,3];
+
 const _map = (func, arr) => {
-  if(!arr.length) return [];
+  const result = [];
   
-  return arr.map((elem, indx) =>  func(elem, indx, arr))
+  if(!arr.length) return result;
+  
+  arr.forEach((elem, indx) =>  result.push(func(elem, indx, arr)))
+  
+  return result;
 }
-//console.log(_map((x) => x + 1, [1,2,3]))
+
+Array.prototype._map = function(func) {
+  const context = this;
+  const result = [];
+  
+  if(!arr.length) return result;
+  
+  context.forEach((elem, indx) => result.push(func(elem, indx, context)))
+  
+  return result
+};
+//console.log(_map((x) => x + 1, arr))
+//console.log(arr._map((x) => x + 2)) 
 
 
   
