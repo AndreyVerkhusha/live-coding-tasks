e8295346f1dafd5afb97e32894115a7aba6354db
AndreyVerkhusha <135759268+AndreyVerkhusha@users.noreply.github.com>
2025-01-17 12:07:52 +0300

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -21,8 +21,34 @@ Array.prototype._map = function(func) {
   
   return result
 };
-//console.log(_map((x) => x + 1, arr))
-//console.log(arr._map((x) => x + 2)) 
+// console.log(_map((x) => x + 1, arr))
+// console.log(arr._map((x) => x + 2)) 
 
+/* 
+Результат должен быть такой, где каждая строка представляет собой либо отдельное число (если диапазон состоит из одного элемента), либо диапазон чисел в формате "start-end".
+Например, для входного массива [1, 4, 5, 2, 3, 9, 8, 11, 0] результатом будет массив ['0-5', '8-9', '11'].
+*/
+const range = (arr) => {
+  const  newArr = [... new Set(arr)].sort((a,b ) => a - b);
+  const result  = [];
+  
+  let start = newArr[0];
+  let end = newArr[0]
+  
+  for(let i = 1; i <= newArr.length; i++) {
+    if(newArr[i] === end + 1) {
+      end = newArr[i]
+    } else {
+      const newElem = start === end ? start.toString() : `${start}-${end}`;
+      result.push(newElem)
+      start = newArr[i]
+      end = newArr[i]
+    }
+  }
+  
+  return result
+};
+// console.log(range([1,4,5,2,3,9,8,11,0])) // '0-5, 8-9, 11'
+// console.log(range([1,4,3,2])) // '1-4'
 
   
